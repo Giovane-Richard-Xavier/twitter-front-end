db81071486a969a79802e0e11a6087154a53c17f
Giovane Richard <giovane@rlvtecnologia.com.br>
2026-09-11 09:11:24 -0300

    feat: implments adjustments in signin-form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
-import { SigninForm } from "@/components/auth/siginin-form";
-import { Button } from "@/components/ui/Button";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/Checkbox";
-import { RadioButton } from "@/components/ui/Radiobutton";
-import { TextArea } from "@/components/ui/TextArea";
+import SigninPage from "./(auth)/signin/page";
 
 export default function Page() {
   function onSubmit() {
@@ -21,10 +8,7 @@ export default function Page() {
   return (
     <main className="w-screen min-h-screen">
       <section className="flex items-center justify-center h-screen">
-        <SigninForm />
-        <SigninForm />
-        <SigninForm />
-        <SigninForm />
+        <SigninPage />
       </section>
     </main>
   );
--- src/components/auth/siginin-form.tsx
+++ src/components/auth/siginin-form.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { IconMail } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "../ui/Button";
 import { Input } from "../ui/Input";
 import { Label } from "../ui/Label";
-import { IconEye, IconEyeOff, IconMail } from "@tabler/icons-react";
 
 export const SigninForm = () => {
   const router = useRouter();
@@ -13,29 +13,39 @@ export const SigninForm = () => {
   const [emailField, setEmailField] = useState("");
   const [passwordField, setPasswordField] = useState("");
 
+  const handleSubmit = () => {
+    console.log("email ->", emailField);
+  };
+
   return (
-    <div className="flex flex-col gap-5 max-w-lg p-8 rounded-sm border border-zinc-200">
-      <Input
-        // label="E-mail"
-        label={<Label required>E-mail</Label>}
-        id="email"
-        placeholder="Digite seu e-mail"
-        type="text"
-        className="w-xs"
-        rightIcon={<IconMail size={18} />}
-      />
+    <div className="flex flex-col gap-6 p-8 rounded-sm border border-zinc-200 mt-10 mb-14">
+      <form action={handleSubmit}>
+        <Input
+          // label="E-mail"
+          label={<Label required>E-mail</Label>}
+          id="email"
+          placeholder="Digite seu e-mail"
+          type="text"
+          className="w-xs"
+          rightIcon={<IconMail size={18} />}
+          value={emailField}
+          onChange={(e) => setEmailField(e.target.value)}
+        />
 
-      <Input
-        // label="Senha"
-        label={<Label required>Senha</Label>}
-        id="password"
-        placeholder="Digite sua senha"
-        type="password"
-        className="w-xs"
-        showPasswordToggle
-      />
+        <Input
+          // label="Senha"
+          label={<Label required>Senha</Label>}
+          id="password"
+          placeholder="Digite sua senha"
+          type="password"
+          className="w-xs"
+          showPasswordToggle
+        />
 
-      <Button className="w-xs mt-5">Entrar</Button>
+        <Button type="submit" className="w-xs mt-5">
+          Entrar
+        </Button>
+      </form>
     </div>
   );
 };
