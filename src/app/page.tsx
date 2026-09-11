716b533138b01a8bcbca927ffb31b09cec24666b
Giovane Richard <giovane@rlvtecnologia.com.br>
2026-09-11 15:22:38 -0300

    feat: implements principal layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import SigninPage from "./(auth)/signin/page";
+import { Logo } from "@/components/ui/logo";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  function onSubmit() {
-    // console.log(data);
-  }
+  // redirect("/home");
 
   return (
-    <main className="w-screen min-h-screen">
+    <main className="w-screen min-h-screen flex items-center justify-center">
       <section className="flex items-center justify-center h-screen">
-        <SigninPage />
+        <Logo size={180} />
       </section>
     </main>
   );
