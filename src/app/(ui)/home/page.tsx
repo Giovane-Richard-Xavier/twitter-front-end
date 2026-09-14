290f519f6398cad13d5922ab0fd493d103bb7670
Giovane Richard <giovane@rlvtecnologia.com.br>
2026-09-14 09:32:06 -0300

    feat: implements home-menu component

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,7 +1,9 @@
+import { HomeHeader } from "@/components/home/home-header";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>HOME</h1>
+      <HomeHeader />
     </div>
   );
 }
